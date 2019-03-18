c000f9a8236e8b65f7b4e53bb6d3ef8091885b53
AlexandreHetic2021 <alexx.monnot@gmail.com>
2019-03-18 14:14:13 +0100

    Header et footer

--- wp-content/themes/hetic/footer.php
+++ wp-content/themes/hetic/footer.php
@@ -1,10 +1,42 @@
-        <footer id="footer">
-        <!--
-            Tout le contenu de la partie head de mon site
-         -->
-        </footer>
+        <footer id="footer" style="background-color :<?php the_field('couleur_footer', 'options'); ?>; color:<?php the_field('couleur_texte_footer', 'options'); ?>; ">
+          
+      <?php wp_footer();  
 
-        <!-- Execution de la fonction wp_footer() obligatoire ! -->
-        <?php wp_footer();  ?>
+       if(get_bloginfo('language') == "fr-FR") : 
+          
+           $a_propos = get_field('a_propos', 'options'); 
+           echo($a_propos['titre']);
+           echo($a_propos['texte']);
+           echo($a_propos['lien']); 
+           echo($a_propos['texte_lien']); 
+          
+      else :
+          
+           $about_us = get_field('about_us', 'options'); 
+           echo($about_us['titre']);
+           echo($about_us['texte']);
+           echo($about_us['lien']); 
+           echo($about_us['texte_lien']); 
+
+      endif; 
+
+
+       wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); 
+
+
+        
+        if( have_rows('reseaux_sociaux', 'options') ):
+          while ( have_rows('reseaux_sociaux', 'options') ) : the_row();
+
+            $logo_reseau_social = get_sub_field('logo_reseau_social');
+            $lien_reseau_social = get_sub_field('lien_reseau_social');
+            echo($logo_reseau_social);
+            echo($lien_reseau_social);
+
+          endwhile;
+        else :
+
+        endif; ?>
+       </footer>
     </body>
 </html>
--- wp-content/themes/hetic/header.php
+++ wp-content/themes/hetic/header.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
+
+<!DOCTYPE html> 
 <html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="<?php the_field('texte_meta_description');?>" />
+    <title>Publishing Sphere - <?php if(get_the_title()): the_title(); else: echo "404"; endif; ?> </title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="icon" href="<?php the_field('favicon', 'options'); ?>"/>
 
-        <!-- Appel du fichier style.css de notre thème -->
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
-        <!--
-            Tout le contenu de la partie head de mon site
-         -->
-
-        <!-- Execution de la fonction wp_head() obligatoire ! -->
-        <?php wp_head(); ?>
-    </head>
-    <body <?php body_class(); ?>>
-        <header id="header">
-        <!-- Tout le contenu de l entête de mon site -->
-        </header>
+    <?php wp_head(); ?>
+</head>
+<header>
+  <body <?php body_class(); ?>>
+     <a href="<?php echo home_url(); ?>">
+            <img src="<?php the_field('logo', 'options'); ?>" alt="<?php the_field('alt_logo', 'options'); ?>">
+     </a>
+  
+          <? wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+          <? wp_nav_menu( array( 'theme_location' => 'language-menu' ) ); ?>
+</header>
