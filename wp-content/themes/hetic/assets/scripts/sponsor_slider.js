06bb25772774bd10568102711c5390d04fc30281
Eliwoodno <pbarraux@gmail.com>
2019-03-22 10:04:30 +0100

    sponsor slider

---
+++ wp-content/themes/hetic/assets/scripts/sponsor_slider.js
@@ -0,0 +1,41 @@
+let next = document.querySelector('.sponsor-slider .next')
+let previous = document.querySelector('.sponsor-slider .previous')
+let slides =  document.querySelectorAll('.sponsor-slider .slide')
+let slider =  document.querySelector('.sponsor-slider')
+
+
+
+
+const sliderNav = () => {
+slides[0].classList.add('active')
+
+let i =0
+previous.addEventListener('click', (event) =>
+{
+    event.stopPropagation()
+    slides[i].classList.remove('active')
+    if(i > 0){
+      i-- 
+    }else{
+      i = slides.length -1
+    }
+    console.log(i)
+    slides[i].classList.add('active')
+})
+
+next.addEventListener('click', (event) =>
+{
+    event.stopPropagation()
+    slides[i].classList.remove('active')
+    if(i < slides.length-1){
+      i++
+    }else{
+      i = 0
+    }
+    console.log(i)
+    slides[i].classList.add('active')
+})
+}
+sliderNav();
+//lol
+
